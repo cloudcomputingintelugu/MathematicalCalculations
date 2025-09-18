cb3296e4d4ed01498c2e78142a8d016eb49eadd5
Naga Srinivasa <cloudcomputingchannel@gmail.com>
2025-09-18 20:38:14 +0530

    Update MathWarAppApplication.java

--- src/main/java/com/example/mathwarapp/MathWarAppApplication.java
+++ src/main/java/com/example/mathwarapp/MathWarAppApplication.java
@@ -2,7 +2,6 @@ package com.example.mathwarapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,10 +9,5 @@ public class MathWarAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MathWarAppApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MathWarAppApplication.class);
     }
 }
